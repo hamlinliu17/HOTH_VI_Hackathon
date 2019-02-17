1bc8d544e8019cb37d1ee9140de795446bf7e5ba
Hamlin Liu <hamlin.liu@gmail.com>
2019-02-17 11:46:28 -0800

    Create 18_19_clean.py

---
+++ data/18_19_clean.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+footballData_18_19 = pd.read_csv("17_18.csv")
+
+footballData_18_19.info()
+
+data_columns = footballData_18_19.columns
+
+index = 0
+while index < len(data_columns):
+    if index >= 23:
+        footballData_18_19.drop([data_columns[index]], axis = 1, inplace = True)
+    index += 1
+
+
+footballData_18_19.head()
