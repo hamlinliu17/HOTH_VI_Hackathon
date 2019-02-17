a27c6e087490e835c8d8afa556a4b3153e8cae9a
Hamlin Liu <hamlin.liu@gmail.com>
2019-02-17 15:48:28 -0800

    New UI

--- UI/backend.py
+++ UI/backend.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('Webapp.html')
+
+@app.route('/generate_results')
+def print_s():
+    home_team = request.args.get('home_team')
+    return home_team
+
+
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
