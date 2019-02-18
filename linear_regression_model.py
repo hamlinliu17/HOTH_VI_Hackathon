3f8be7cd274a40db7673e904e95842fbe8c43356
shriniketbuche <shriniketbuche@gmail.com>
2019-02-17 19:36:12 -0800

    Fixed Draw with uneven goals

--- linear_regression_model.py
+++ linear_regression_model.py
@@ -6,7 +6,7 @@ from data_clean_18_19 import *
 
 Input_Matrix_train, Input_Matrix_test, Result_Matrix_train, Result_Matrix_test = train_test_split(Input_Matrix, Result_Matrix)
 
-model = RandomForestClassifier(n_estimators = 100)
+model = RandomForestClassifier(n_estimators = 1000)
 
 model.fit(Input_Matrix_train, Result_Matrix_train)
 
@@ -26,7 +26,12 @@ def winner(x1,x2):
         return 50
     elif(int(predict(x1,x2)[2]) == 1):
         return x1
-    elif(int(predict(x1,x2)[2]) == 0):
-        return 50
-    else:
+    elif(int(predict(x1,x2)[2]) == -1):
         return x2
+    else:
+        if(predict(x1,x2)[0] < predict(x1,x2)[1]):
+            return x2
+        elif(predict(x1,x2)[0] > predict(x1,x2)[1]):
+            return x1
+        else:
+            return 50
