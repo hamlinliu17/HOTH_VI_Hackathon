f250c0b16b29baf9017223f9ad8aa8d53b6e5e0c
Hamlin Liu <hamlin.liu@gmail.com>
2019-02-17 16:28:13 -0800

    finishing

--- backend.py
+++ backend.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, request
+from data.linear_regression_model import*
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('Webapp.html')
 
 @app.route('/generate_results')
-def print_s():
+def home_team():
     home_team = request.args.get('home_team')
     return home_team
+def visit_team():
+    away_team = request.args.get('away_team')
+    return away_team
+
+def results():
+    result = winnter(home_team(), visit_team())
+    return result
 
 
 
