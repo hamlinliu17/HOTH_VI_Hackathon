58b4e58525d63e4a827071148f589b7bac1dc1a6
Hamlin Liu <hamlin.liu@gmail.com>
2019-02-17 17:13:16 -0800

    Fixing UI

--- backend.py
+++ backend.py
@@ -1,16 +1,67 @@
 from flask import Flask, render_template, request
-from data import winner
+from linear_regression_model import *
+
+
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('Webapp.html')
 
 @app.route('/generate_results')
+
 def results():
     home_team = request.args.get('home_team')
     away_team = request.args.get('away_team')
-    result = winner(home_team, away_team)
-    return result
+    value = winner(home_team, away_team)
+    if value == 0:
+        return "Arsenal"
+    elif  value ==  1:
+        return "Bournemoth"
+    elif value == 3:
+        return "Burnley"
+    elif value == 5:
+        return "Chelsea"
+    elif value == 6:
+        return "Crystal Palace"
+    elif value == 7:
+        return "Everton"
+    elif value == 24:
+        return "Hull City"
+    elif value == 10:
+        return "Leicester City"
+    elif value == 11:
+        return "Liverpool"
+    elif value == 12:
+        return "Manchester City"
+    elif value == 13:
+        return "Manchester United"
+    elif value == 25:
+        return "Middlesborough"
+    elif value == 15:
+        return "Southampton"
+    elif value == 20:
+        return "Stoke City"
+    elif value == 26:
+        return "Sunderland"
+    elif value == 21:
+        return "Swansea City"
+    elif value == 16:
+        return "Tottenham"
+    elif value == 2:
+        return "Watford"
+    elif value == 23:
+        return "West Bromwich"
+    elif value == 18:
+        return "West Ham United"
+    elif value == 2:
+        return "Brighton"
+    elif value == 9:
+        return "Huddersfeld"
+    elif value == 50:
+        return "draw"
+    else:
+        return value
+
 
 
 
---
+++ linear_regression_model.py
@@ -0,0 +1,27 @@
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+import numpy as np
+from data_clean_18_19 import *
+
+Input_Matrix_train, Input_Matrix_test, Result_Matrix_train, Result_Matrix_test = train_test_split(Input_Matrix, Result_Matrix)
+
+model = RandomForestClassifier(n_estimators = 100)
+
+model.fit(Input_Matrix_train, Result_Matrix_train)
+
+def predict(x1,x2):
+    if(x1 == x2):
+        return 0
+    #Result_Matrix_Prediction = [[]]
+    Result_Matrix_Prediction = model.predict([[x1,x2]])
+    result = Result_Matrix_Prediction[0][2]
+    return result
+
+def winner(x1,x2):
+    if(predict(x1,x2) == 1):
+        return x1
+    elif(predict(x1,x2) == 0):
+        return 50
+    else:
+        return x2
